8ad52c9bb0c29091c59c927e5ded11fb71396233
Paolo Sivila <paspau2@hotmail.com>
2024-04-13 22:29:35 -0500

    Added Candles

--- js/Game.js
+++ js/Game.js
@@ -3,31 +3,67 @@ class Game extends Phaser.Scene {
 		super('game');
 	}
 
+	init(){
+		let candle1_state = false;
+		let candle2_state = false;
+		let candle3_state = false;
+		let candle4_state = false;
+		let candle5_state = false;
+	}
+
 	create(){
 		console.log('Game started');
 
 		this.drawGrid();
-		this.drawGlyphs();
 	}
 
 	drawGrid() {
 		console.log('Drawing the grid');
-		
+		let bgFloor = this.add.image(0,0, 'bg-floor');
+		bgFloor.setOrigin(0,0);
+
+		let bgCircle = this.add.image(0,0, 'bg-circle');
+		bgCircle.setOrigin(0,0);
+
+		//let bgBoard = this.add.image(0,0, 'bg-board');
+		//bgBoard.setOrigin(0,0);
 	}
 
-	drawGlyphs(){
-		const frames = this.textures.get('glyph_test').getFrameNames();
-		const glyphs = [];
-		for (var i=0; i<16; i++) {
-			glyphs.push(this.add.sprite(0, 0, 'glyph_test', Phaser.Math.RND.pick(frames)));
+	drawCandles(){
+		if (this.candle1_state == true){
+			let candle1 = this.add.sprite(430, 100, 'candle-on');
+			//this.candle1_state = false;
+		} else {
+			let candle1 = this.add.sprite(430, 100, 'candle-off');
+			//this.candle1_state = true;
 		}
-		Phaser.Actions.GridAlign(glyphs, {
-            width: 4,
-            height: 4,
-            cellWidth: 300,
-            cellHeight: 150,
-            x: 100,
-            y: 100
-    	});
+
+		if (this.candle2_state == true){
+			let candle2 = this.add.sprite(844, 92, 'candle-on');
+		} else {
+			let candle2 = this.add.sprite(844, 92, 'candle-off');
+		}
+
+		if (this.candle3_state == true){
+			let candle3 = this.add.sprite(364, 412, 'candle-on');
+		} else {
+			let candle3 = this.add.sprite(364, 412, 'candle-off');
+		}
+
+		if (this.candle4_state == true){
+			let candle4 = this.add.sprite(932, 386, 'candle-on');
+		} else {
+			let candle4 = this.add.sprite(932, 386, 'candle-off');
+		}
+
+		if (this.candle5_state == true){
+			let candle5 = this.add.sprite(0,0, 'candle-on');
+		} else {
+			let candle5 = this.add.sprite(628, 586, 'candle-off');
+		}
+	}
+
+	update(){
+		this.drawCandles();
 	}
 }
--- js/Load.js
+++ js/Load.js
@@ -11,8 +11,14 @@ class Load extends Phaser.Scene {
 		this.load.image('bg-room', 'asset/img/MyDemonicLittleDemons_Gameplay.png');
 		this.load.image('bg-menu', 'asset/img/MyDemonicLittleDemons_Menu.png');
 		this.load.image('btn-play', 'asset/img/mdld_Menu_btn-play.png');
+		this.load.image('bg-board', 'asset/img/MyDemonicLittleDemons_Board.png');
+		this.load.image('candle-off', 'asset/img/T_MDLD_Candle_off.png');
+		this.load.image('candle-on', 'asset/img/T_MDLD_Candle_on.png');
+		this.load.image('bg-floor', 'asset/img/T_MDLD_Bg_Floor.png');
+		this.load.image('bg-circle', 'asset/img/T_MDLD_Circle.png');
 
 		this.load.spritesheet('glyph_test', 'asset/img/ss_glyphs_test.png', {frameWidth: 96, frameHeight: 128});
+		this.load.spritesheet('grid-test', 'asset/img/mdld_grid-test.png', {frameWidth: 128, frameHeight: 128});
 	}
 
 	create(){
