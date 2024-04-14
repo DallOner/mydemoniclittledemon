392a346e955c7ef53fbef47e24135c7eaf6687f4
Paolo Sivila <paspau2@hotmail.com>
2024-04-14 16:34:57 -0500

    Added table in the invoke scene

--- js/Invoke.js
+++ js/Invoke.js
@@ -15,8 +15,29 @@ class Invoke extends Phaser.Scene {
         const room = this.add.image(0, 0, 'bg-gameplay');
         room.setOrigin(0);
 
-        const rm_shadow = this.add.image(0, 0, 'bg-shadow');
-        rm_shadow.setOrigin(0);
+        const portrait1 = this.add.image(320, 20, 'portrait-1');
+        portrait1.setOrigin(0);
+
+        const portrait2 = this.add.image(780, 0, 'portrait-2');
+        portrait2.setOrigin(0);
+
+        const cat = this.add.image(166, 512, 'cat');
+        cat.setOrigin(0);
+
+        const cross = this.add.image(532, 0, 'cross');
+        cross.setOrigin(0);
+
+        const curtain = this.add.image(1016, 0, 'curtain');
+        curtain.setOrigin(0);
+
+        const lamp = this.add.image(860, 88, 'lamp');
+        lamp.setOrigin(0);
+
+        const table = this.add.image(224, 320, 'table');
+        table.setOrigin(0);
+
+        //const rm_shadow = this.add.image(0, 0, 'bg-shadow');
+        //rm_shadow.setOrigin(0);
 
         //Shows the animation...
         setTimeout(() => {
