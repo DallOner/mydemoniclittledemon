8d9acfb59e64946719145d2eb79e05f8c313ee54
Paolo Sivila <paspau2@hotmail.com>
2024-04-15 19:16:39 -0500

    Update Rebalance the game.

--- js/Game.js
+++ js/Game.js
@@ -15,7 +15,7 @@ class Game extends Phaser.Scene {
         this.spacer = null;
         this.slideSpeed = 300;
         this.slideEase = 'power3';
-        this.iterations = 2;
+        this.iterations = 10;
 
         //  The speed at which the pieces are shuffled at the start. This allows
         //  the player to see the puzzle before trying to solve it. However if
@@ -474,7 +474,7 @@ class Game extends Phaser.Scene {
 
         if (this.photo === 'gly-01'){
             nextPhoto = 'gly-02';
-            iterations = 2;
+            iterations = 16;
             size = 4;
         } else if (this.photo === 'gly-02'){
             nextPhoto = 'gly-03';
